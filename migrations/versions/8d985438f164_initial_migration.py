638c35aee737f0c212e59d6e86801ae62ab9e05c
Joel Castillo <jocastillo@records.nyc.gov>
2019-04-20 10:12:58 -0400

    Add isort back

--- migrations/versions/8d985438f164_initial_migration.py
+++ migrations/versions/8d985438f164_initial_migration.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2019-04-20 02:27:25.030152
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "8d985438f164"
